2c27074c3fb395f8c957ed675ecc8deaa8a4d640
gunnarsikorski <gunnarsikorski@gmail.com>
2021-04-13 14:02:42 -0500

    Adding multiple markers of same type

--- map.py
+++ map.py
@@ -17,6 +17,21 @@ fav_foods.add_child(folium.Marker(
 
 map.add_child(fav_foods)
 
+
+cub_foods = folium.FeatureGroup(name = 'Good Cub Locations')
+
+cub_locations = [[44.97089515982077, -93.34941680718995],
+                 [45.00683148574318, -93.2321720985536],
+                 [44.882780266340134, -93.32040603725443]]
+
+for coords in cub_locations:
+    cub_foods.add_child(folium.Marker(
+        location = coords,
+        popup = 'Cub Foods',
+        icon = folium.Icon(color = 'red')))
+
+map.add_child(cub_foods)
+
 #_________________
 
 map.save('map1.html')
