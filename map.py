13ab2dcaa3ec21fbf69a1e5c398898577a808ac0
gunnarsikorski <gunnarsikorski@gmail.com>
2021-04-13 13:49:22 -0500

    Learning markers and adding them

--- map.py
+++ map.py
@@ -1,4 +1,22 @@
 import folium
 
-map = folium.Map(location = [44.986, -93.269], zoom_start = 10)
+map = folium.Map(
+    location = [44.986, -93.269],
+    zoom_start = 10,
+    tiles = 'Stamen Terrain')
+
+#_________________
+## MARKERS ##
+
+fav_foods = folium.FeatureGroup(name = 'Favorite Eateries')
+
+fav_foods.add_child(folium.Marker(
+    location = [44.95286, -93.28779],
+    popup = 'Milkjam Creamery',
+    icon = folium.Icon(color='purple')))
+
+map.add_child(fav_foods)
+
+#_________________
+
 map.save('map1.html')
